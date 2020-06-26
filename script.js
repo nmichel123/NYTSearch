a192e2c655ad93515c136b7954e2550694237f38
John Kim <johnk9000@gmail.com>
2020-06-26 13:56:19 -0500

    Let's make this the 'master' script

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-
 var article = $(this).attr("data-name");
 var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + article + "&api-key=TONEZkf1tgn51V71vK11iTJ1gyMdmxqR";
+
+$.ajax({
+
+    'type': 'GET',
+    'url': queryurl,
+    data: {
+        'q': queryString,
+        'response-format': "jsonp",
+        'api-key': nytApiKey,
+        'callback': 'svc_search_v2_articlesearch'
+    },
+        success: function(data) {
+        // passed function object for data processing 
+        console.log(data);
+
+        
+    }
+});
+
+//retrieve data from ajax 
+
+//then create div's recursively
+
+//popululate search result divs with retreived data
